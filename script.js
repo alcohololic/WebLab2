1398a486016243913bd9c5cc4e45ac9d287f9690
alcohololic <vlad.timofeev97@gmail.com>
2018-05-18 00:43:42 +0300

    Add check and code refactoring

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function checkf() {
-
-    validate(form_fname, /^[A-Za-zА-Яа-я]+$/) ? succesMessage(form_fname) : errorMessage(form_fname);
-    validate(form_sname, /^[A-Za-zА-Яа-я]+$/) ? succesMessage(form_sname) : errorMessage(form_sname);
-    validate(form_address, /\d{5}/) ? succesMessage(form_address) : errorMessage(form_address);
-    validate(form_email, '@') ? succesMessage(form_email) : errorMessage(form_email);
+function checkf(event) {
+    event.preventDefault();
+    validate(form_fname, /^[A-Za-zА-Яа-я]+$/) ? successMessage(form_fname) : errorMessage(form_fname);
+    validate(form_sname, /^[A-Za-zА-Яа-я]+$/) ? successMessage(form_sname) : errorMessage(form_sname);
+    validate(form_address, /\d{5}/) ? successMessage(form_address) : errorMessage(form_address);
+    validate(form_email, '@') ? successMessage(form_email) : errorMessage(form_email);
 
 }
 
@@ -21,6 +21,6 @@ function errorMessage(id) {
     id.value = 'Невірно введені дані';
 }
 
-function succesMessage(id) {
+function successMessage(id) {
     id.style.border = '1px solid green';
 }
