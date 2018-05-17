10313d3d9098ffcc4938418ff0a8b7d0f657919a
alcohololic <vlad.timofeev97@gmail.com>
2018-05-18 00:49:56 +0300

    Remove code dublicating

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 function checkf(event) {
     event.preventDefault();
-    validate(form_fname, /^[A-Za-zА-Яа-я]+$/) ? successMessage(form_fname) : errorMessage(form_fname);
-    validate(form_sname, /^[A-Za-zА-Яа-я]+$/) ? successMessage(form_sname) : errorMessage(form_sname);
-    validate(form_address, /\d{5}/) ? successMessage(form_address) : errorMessage(form_address);
-    validate(form_email, '@') ? successMessage(form_email) : errorMessage(form_email);
+    validate(form_fname, /^[A-Za-zА-Яа-я]+$/);
+    validate(form_sname, /^[A-Za-zА-Яа-я]+$/);
+    validate(form_address, /\d{5}/);
+    validate(form_email, '@');
 
 }
 
-function validate(item, condition) {
-    if (!item.value.match(condition)) {
-        return false;
-    }
-    else {
-        return true;
-    }
+function validate(id, condition) {
+    (id.value.match(condition)) ? successMessage(id) : errorMessage(id);
 }
 
 function errorMessage(id) {
@@ -24,3 +19,4 @@ function errorMessage(id) {
 function successMessage(id) {
     id.style.border = '1px solid green';
 }
+
